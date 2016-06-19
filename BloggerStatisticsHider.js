74e8c1d4d308bc8bc8ddba8f584e60201250d66d
Si Yuan <easy0519@gmail.com>
2016-06-19 23:25:09 +0800

    加入新規格，修正每日流量顯示

--- BloggerStatisticsHider.js
+++ BloggerStatisticsHider.js
@@ -2,7 +2,7 @@
 // @name         Blogger Statistics Hider
 // @name:zh-TW   Blogger 流量資訊隱藏
 // @namespace    http://www.twsiyuan.com/
-// @version      0.1
+// @version      0.11
 // @description  Hide all statistics info in blogger
 // @description:zh-TW  在 Blogger 管理頁面中，隱藏所有的流量統計資訊
 // @author       Siyuan
@@ -20,5 +20,7 @@
     sheet.insertRule('.DY0BBXD-p-d a:nth-child(2) { display: none !important; }', sheet.cssRules.length);
     sheet.insertRule('.OJTUNIC-F-g { display: none !important; }', sheet.cssRules.length);
     sheet.insertRule('.DY0BBXD-p-m { display: none !important; }', sheet.cssRules.length);
-   
+    sheet.insertRule('.DY0BBXD-p-m { display: none !important; }', sheet.cssRules.length);
+    sheet.insertRule('.editPosts tr td:nth-child(7) { display: none !important; }', sheet.cssRules.length);   
+    
 })();
