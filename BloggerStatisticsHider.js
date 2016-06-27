1ac2baf508edb9483633e5b45bd6c0cc0f67aa20
Si Yuan <easy0519@gmail.com>
2016-06-27 08:11:08 +0800

    修改實做方式，以因應每次都不同的 ClassStyle 的版面

--- BloggerStatisticsHider.js
+++ BloggerStatisticsHider.js
@@ -2,25 +2,44 @@
 // @name         Blogger Statistics Hider
 // @name:zh-TW   Blogger 流量資訊隱藏
 // @namespace    http://www.twsiyuan.com/
-// @version      0.12
+// @version      0.2
 // @description  Hide all statistics info in blogger
 // @description:zh-TW  在 Blogger 管理頁面中，隱藏所有的流量統計資訊
 // @author       Siyuan
 // @match        https://www.blogger.com/home
 // @match        https://www.blogger.com/blogger.g*
 // @grant        none
+// @require      http://code.jquery.com/jquery-latest.js
 // ==/UserScript==
 
 (function() {
     'use strict';
     
+    $('img.blogg-button-image').each(function() {
+        var container = $(this).parent().parent().parent();
+        container.find('img').each(function() {
+            if ($(this).attr('src').startsWith('//chart')){
+                $(this).hide();
+            }
+        });
+        container.find('a').each(function() {
+            var href = $(this).attr('href');
+            if (href && href.indexOf('#overviewstats') >= 0){
+                $(this).hide();
+            }
+        });
+    });
+    
+    
+    $('div.overview a').each(function() {
+        var href = $(this).attr('href');
+        if (href && href.indexOf('#overviewstats') >= 0){
+            $(this).text('');
+        }
+    });
+    
     var sheet = window.document.styleSheets[0];
-    sheet.insertRule('.gwt-Image { display: none !important; }', sheet.cssRules.length);
-    sheet.insertRule('.OJTUNIC-q-A tr:nth-child(3) { display: none !important; }', sheet.cssRules.length);
-    sheet.insertRule('.DY0BBXD-p-d a:nth-child(2) { display: none !important; }', sheet.cssRules.length);
-    sheet.insertRule('.OJTUNIC-F-g { display: none !important; }', sheet.cssRules.length);
-    sheet.insertRule('.DY0BBXD-p-m { display: none !important; }', sheet.cssRules.length);
-    sheet.insertRule('.DY0BBXD-p-m { display: none !important; }', sheet.cssRules.length);
+    sheet.insertRule('.gwt-Image { visibility: hidden !important; }', sheet.cssRules.length);
     sheet.insertRule('.editPosts tr td:nth-child(7) { display: none !important; }', sheet.cssRules.length);   
     sheet.insertRule('.pages tr td:nth-child(7) { display: none !important; }', sheet.cssRules.length);
 })();
