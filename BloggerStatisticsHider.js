ad9bf6826cf53d10df6d79f670c875b9fa6037dd
Si Yuan <easy0519@gmail.com>
2016-11-18 17:20:56 +0800

    Update to 1.1, fixed some viewing bugs

--- BloggerStatisticsHider.js
+++ BloggerStatisticsHider.js
@@ -2,7 +2,7 @@
 // @name         Blogger Statistics Hider
 // @name:zh-TW   Blogger 流量資訊隱藏
 // @namespace    http://www.twsiyuan.com/
-// @version      1.0
+// @version      1.1
 // @description  Hide all statistics info in blogger
 // @description:zh-TW  在 Blogger 管理頁面中，隱藏所有的流量統計資訊
 // @author       Siyuan
@@ -12,34 +12,18 @@
 // @require      http://code.jquery.com/jquery-latest.js
 // ==/UserScript==
 
-(function() {
+$(document).ready(function() {
     'use strict';
-    
-    $('img.blogg-button-image').each(function() {
-        var container = $(this).parent().parent().parent();
-        container.find('img').each(function() {
-            if ($(this).attr('src').startsWith('//chart')){
-                $(this).hide();
-            }
-        });
-        container.find('a').each(function() {
-            var href = $(this).attr('href');
-            if (href && href.indexOf('#overviewstats') >= 0){
-                $(this).hide();
-            }
-        });
-    });
-    
-    
-    $('div.overview a').each(function() {
+
+    $('a').each(function() {
         var href = $(this).attr('href');
         if (href && href.indexOf('#overviewstats') >= 0){
-            $(this).text('');
+            $(this).hide();
         }
     });
-    
+
     var sheet = window.document.styleSheets[0];
     sheet.insertRule('.gwt-Image { visibility: hidden !important; }', sheet.cssRules.length);
     sheet.insertRule('.editPosts tr td:nth-child(7) { display: none !important; }', sheet.cssRules.length);   
     sheet.insertRule('.pages tr td:nth-child(7) { display: none !important; }', sheet.cssRules.length);
-})();
+});
